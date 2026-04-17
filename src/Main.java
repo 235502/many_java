7bdbac67b882dedc672aaa99831a451c8e8c3761
235502 <235502@example.com>
2026-04-17 17:35:08 +0800

    Changes

--- src/Main.java
+++ src/Main.java
@@ -2,14 +2,31 @@
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        System.out.print("Hello and welcome!");
+        System.out.println("欢迎使用成绩管理系统\n");
+        System.out.println("***********************************************");
+        System.out.println("               1. 学生信息管理\t");
+        System.out.println("               2. 教师信息管理\t");
+        System.out.println("               3. 成绩录入\t");
+        System.out.println("               4. 成绩查询\t");
+        System.out.println("               5. 成绩修改\t");
+        System.out.println("               6. 返回上一级\t");
+        System.out.println("***********************************************");
+        System.out.print("请选择, 输入数字：");
 
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            System.out.println("i = " + i);
+        String[] heart = {
+                "  *   *   *   *",
+                " *     * *      *",
+                "*       *       *",
+                " *             *",
+                "  *           *",
+                "    *       *",
+                "      *   *",
+                "        *",
+
+        };
+        for (String line : heart) {
+            System.out.println(line);
+        }
+
         }
     }
-}
